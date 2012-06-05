63b894c361dc5001088fdd009941762a3adddb53
Tully Foote <tfoote@willowgarage.com>
2012-06-04 17:13:01 -0700

    0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='bloom',
-      version='0.0.5',
+      version='0.0.6',
       packages=['bloom'],
       package_dir = {'bloom': 'src/bloom'},
       scripts = ['bin/bloom-generate-debian',
