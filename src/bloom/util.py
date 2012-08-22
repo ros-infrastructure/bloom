e6f12e6a96c42359a8b453d9cb8f76871e02b1e0
William Woodall <wjwwood@gmail.com>
2012-08-22 15:02:36 -0700

    Added support for ansi color escape sequences.

--- src/bloom/util.py
+++ src/bloom/util.py
@@ -1,56 +1,71 @@
-from tempfile import mkdtemp
+"""
+Provides common utility functions for bloom.
+"""
 import sys
 
-TMPDIR = mkdtemp(prefix='bloom_')
+_ansi = {}
 
-ansi = {}
+
+def ansi(key):
+    """Returns the escape sequence for a given ansi color key"""
+    global _ansi
+    return _ansi[key]
+
+
+def create_temporary_directory(prefix_dir=None):
+    """Creates a temporary directory and returns its location"""
+    from tempfile import mkdtemp
+    return mkdtemp(prefix='bloom_', dir=prefix_dir)
 
 
 def enable_ANSI_colors():
-    '''
+    """
     Populates the global module dictionary `ansi` with ANSI escape sequences.
-    '''
-    global ansi
+    """
+    global _ansi
     colors = [
         'black', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white'
     ]
-    ansi = {
+    _ansi = {
         'escape': '\033', 'reset': 0,
         'boldon': 1, 'italicson': 3, 'ulon': 4, 'invon': 7,
         'boldoff': 22, 'italicsoff': 23, 'uloff': 24, 'invoff': 27,
     }
 
+    # Convert plain numbers to escapes
+    for key in _ansi:
+        if key != 'escape':
+            _ansi[key] = '{0}[{1}m'.format(_ansi['escape'], _ansi[key])
+
     # Foreground
     for index, color in enumerate(colors):
-        ansi[color + 'f'] = '{0}[{1}m'.format(ansi['escape'], 30 + index)
+        _ansi[color + 'f'] = '{0}[{1}m'.format(_ansi['escape'], 30 + index)
 
     # Background
     for index, color in enumerate(colors):
-        ansi[color + 'b'] = '{0}[{1}m'.format(ansi['escape'], 40 + index)
+        _ansi[color + 'b'] = '{0}[{1}m'.format(_ansi['escape'], 40 + index)
 
 
 def disable_ANSI_colors():
-    '''
+    """
     Sets all the ANSI escape sequences to empty strings, effectively disabling
     console colors.
-    '''
-    global ansi
-    for index in range(len(ansi)):
-        ansi[index] = ''
+    """
+    global _ansi
+    for key in _ansi:
+        _ansi[key] = ''
 
 enable_ANSI_colors()
 
 
 def maybe_continue(default='y'):
-    '''Prompts the user for continuation'''
-    global ansi
-
+    """Prompts the user for continuation"""
     default = default.lower()
-    msg = "{0}Continue ".format(ansi['boldon'])
+    msg = "{0}Continue ".format(ansi('boldon'))
     if default is 'y':
-        msg += "{0}[Y/n]?{1}".format(ansi['yellowf'], ansi['reset'])
+        msg += "{0}[Y/n]? {1}".format(ansi('yellowf'), ansi('reset'))
     else:
-        msg += "{0}[y/N]?{1}".format(ansi['yellowf'], ansi['reset'])
+        msg += "{0}[y/N]? {1}".format(ansi('yellowf'), ansi('reset'))
 
     while True:
         response = raw_input(msg)
@@ -59,9 +74,9 @@ def maybe_continue(default='y'):
 
         response = response.lower()
         if response not in ['y', 'n']:
-            error_msg = ansi['redf'] + 'Reponse `' + response
+            error_msg = ansi('redf') + 'Reponse `' + response
             error_msg += '` was not recognized, please use one of y, Y, n, N.'
-            error_msg += ansi['reset']
+            error_msg += ansi('reset')
             print(error_msg)
         else:
             break
@@ -70,7 +85,27 @@ def maybe_continue(default='y'):
         bailout("Exiting.")
 
 
+def get_version():
+    """Returns the version number from a given tag/branch"""
+    pass  # TODO: implement this
+
+
+def get_upstream_version():
+    """Returns the upstream version number from a given upstream tag/branch"""
+    pass  # TODO: implement this
+
+
 def bailout(reason='Exiting.'):
-    '''Exits bloom for a given reason'''
-    print(ansi['redf'] + ansi['boldon'] + reason + ansi['reset'])
+    """Exits bloom for a given reason"""
+    print(ansi('redf') + ansi('boldon') + reason + ansi('reset'))
     sys.exit(1)
+
+
+def read_stack_xml(file_path):
+    """
+    Returns a dictionary representation of a stack xml file.
+
+    :param file_path: path to stack xml file to be converted
+    :returns: dictionary representation of the stack xml file
+    """
+    pass  # TODO: implement this
