ec74e18be0c2389873af86fc7d4b1ea9140b58f8
William Woodall <wjwwood@gmail.com>
2012-08-27 21:05:01 -0700

    Cleanup of the generate_debian module.

--- src/bloom/generate_debian.py
+++ src/bloom/generate_debian.py
@@ -1,4 +1,35 @@
-#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2012, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 from __future__ import print_function
 
 import pkg_resources
@@ -36,30 +67,6 @@ Version is the upstream version
 DebianInc is some number that is incremental for package maintenance
 Changes is a bulleted list of changes.
 '''
-
-
-def parse_options():
-    import argparse
-    parser = argparse.ArgumentParser(description='Creates/updates a gpb from '
-                                                 'a catkin project.')
-    parser.add_argument('--working', help='A scratch build path. Default: '
-                                          '%(default)s',
-                        default='.tmp/%u/' % os.getpid())
-    parser.add_argument('--debian-revision', dest='debian_revision',
-                        help='Bump the changelog debian number.'
-                             ' Please enter a monotonically increasing number '
-                             'from the last upload.',
-                        default=0)
-    parser.add_argument('--install-prefix', dest='install_prefix',
-                        help='The full prefix ...')
-    parser.add_argument('--distros', nargs='+',
-                        help='A list of debian distros. Default: %(default)s',
-                        default=[])
-
-    #ros specific stuff.
-    parser.add_argument('rosdistro', help='The ros distro. electric, '
-                                          'fuerte, groovy')
-    return parser.parse_args()
 
 
 def call(working_dir, command, pipe=None):
@@ -332,7 +339,8 @@ def main(args):
         rosdep_key = str(e)
         if not isinstance(e, KeyError):
             rosdep_key = e.rosdep_key
-        print("""Cannot resolve dependency [{0}].
+        print("""\
+Cannot resolve dependency [{0}].
 
 If [{1}] is catkin project, make sure it has been added to the gbpdistro file.
 
@@ -341,6 +349,3 @@ rosdep.yaml entry for it in your sources.
 """.format(rosdep_key), file=sys.stderr)
         sys.exit(1)
     sys.exit(0)
-
-if __name__ == "__main__":
-    main(parse_options())
