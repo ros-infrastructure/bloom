844b91c3e55bb16ef7c9fee3bb381a33b4d8422c
William Woodall <wjwwood@gmail.com>
2012-09-18 23:40:12 -0700

    Implemented export verb of git-bloom-patch

--- src/bloom/patch/export_cmd.py
+++ src/bloom/patch/export_cmd.py
@@ -3,9 +3,45 @@ from __future__ import print_function
 import sys
 from argparse import ArgumentParser
 
+from .. util import execute_command
+from .. logging import error
+from .. logging import info
+from .. git import get_branches
+from .. git import get_current_branch
 
-def export_patches(base_branch, cwd=None, src_branch=None):
-    print(base_branch)
+from . common import get_patches_info
+from . common import list_patches
+
+
+def export_patches(directory=None):
+    # Get current branch
+    current_branch = get_current_branch(directory)
+    # Construct the patches branch name
+    patches_branch = 'patches/' + current_branch
+    # Ensure the patches branch exists
+    if patches_branch not in get_branches():
+        error("The patches branch ({0}) does not ".format(patches_branch) + \
+              "exist, did you use git-bloom-branch?")
+        return 1
+    try:
+        # Get parent branch and base commit from patches branch
+        parent_branch, _ = get_patches_info(patches_branch, directory)
+        # Checkout to the patches branch
+        execute_command('git checkout ' + patches_branch, cwd=directory)
+        # Notify the user
+        info("Exporting patches from "
+             "{0}...{1}".format(parent_branch, current_branch))
+        # Create the patches using git format-patch
+        cmd = "git format-patch -M -B " \
+              "{0}...{1}".format(parent_branch, current_branch)
+        execute_command(cmd, cwd=directory)
+        # Report of the number of patches created
+        patches_list = list_patches(directory)
+        info("Created {0} patches".format(len(patches_list)))
+    finally:
+        if current_branch:
+            execute_command('git checkout ' + current_branch, cwd=directory)
+    return 0
 
 
 def get_parser():
@@ -13,12 +49,10 @@ def get_parser():
     parser = ArgumentParser(
         description="""\
 Exports the commits that have been made on the current branch since the
-specified BASE_BRANCH to a patches branch, which is named
-'patches/<current branch name>', using git format-patch.
+original parent branch (source branch from git-bloom-branch) to a patches
+branch, which is named 'patches/<current branch name>', using git format-patch.
 """
     )
-    add = parser.add_argument
-    add('base', metavar="BASE_BRANCH", help="name of branch to patch since")
     return parser
 
 
@@ -27,4 +61,5 @@ def main():
     sysargs = sys.argv[2:]
     parser = get_parser()
     args = parser.parse_args(sysargs)
-    return export_patches(args.base)
+    args  # pylint
+    return export_patches()
