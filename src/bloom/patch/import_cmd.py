90d5617490afb8853b393d7ada05cbadb58d705d
William Woodall <wjwwood@gmail.com>
2012-09-18 23:40:28 -0700

    Implemented import verb of git-bloom-patch

---
+++ src/bloom/patch/import_cmd.py
@@ -0,0 +1,89 @@
+from __future__ import print_function
+
+import sys
+import os
+import tempfile
+import shutil
+from argparse import ArgumentParser
+
+from .. util import execute_command
+from .. logging import error
+from .. logging import info
+from .. logging import warning
+from .. git import get_branches
+from .. git import get_commit_hash
+from .. git import get_current_branch
+from .. git import track_branches
+
+from . common import get_patches_info
+from . common import list_patches
+
+
+def import_patches(directory=None):
+    # Get current branch
+    current_branch = get_current_branch(directory)
+    # Construct the patches branch name
+    patches_branch = 'patches/' + current_branch
+    # Ensure the patches branch exists and is tracked
+    if patches_branch in get_branches():
+        if patches_branch not in get_branches(local_only=True):
+            track_branches(patches_branch)
+    else:
+        error("The patches branch ({0}) does not ".format(patches_branch) + \
+              "exist, did you use git-bloom-branch?")
+        return 1
+    # Create a swap space
+    tmp_dir = tempfile.mkdtemp()
+    try:
+        # Get parent branch and base commit from patches branch
+        parent_branch, commit = get_patches_info(patches_branch, directory)
+        if commit != get_commit_hash(current_branch, directory):
+            warning("The current commit is not the same as the most recent "
+                    "rebase commit. This might mean that you have committed "
+                    "since the last time you did 'git-bloom-patch export'.")
+            return 1
+        # Checkout to the patches branch
+        execute_command('git checkout ' + patches_branch, cwd=directory)
+        # Copy the patches to a temp location
+        patches = list_patches(directory)
+        if len(patches) == 0:
+            warning("No patches in the patches branch, nothing has changed.")
+            return 1
+        tmp_dir_patches = []
+        for patch in patches:
+            tmp_dir_patches.append(os.path.join(tmp_dir, patch))
+            if directory is not None:
+                patch = os.path.join(directory, patch)
+            shutil.copy(patch, tmp_dir)
+        # Now checkout back to the original branch and import them
+        execute_command('git checkout ' + current_branch, cwd=directory)
+        cmd = 'git am {0}*.patch'.format(tmp_dir + os.sep)
+        execute_command(cmd, cwd=directory)
+        # Notify the user
+        info("Applied {0} patches".format(len(patches)))
+    finally:
+        if current_branch:
+            execute_command('git checkout ' + current_branch, cwd=directory)
+        if os.path.exists(tmp_dir):
+            shutil.rmtree(tmp_dir)
+    return 0
+
+
+def get_parser():
+    """Returns a parser.ArgumentParser with all arguments defined"""
+    parser = ArgumentParser(
+        description="""\
+Imports the patches from the patches branch, which is named
+'patches/<current branch name>', onto the current working branch.
+"""
+    )
+    return parser
+
+
+def main():
+    # Assumptions: in a git repo, this command verb was passed, argv has enough
+    sysargs = sys.argv[2:]
+    parser = get_parser()
+    args = parser.parse_args(sysargs)
+    args  # pylint
+    return import_patches()
