16c26c66a15fbb280adb7111366ff6351d8a45a7
William Woodall <wjwwood@gmail.com>
2012-10-05 23:30:06 -0700

    Preliminary debian generator

--- bloom/generators/debian/__init__.py
+++ bloom/generators/debian/__init__.py
@@ -2,7 +2,28 @@ from __future__ import print_function
 
 from bloom.generators import BloomGenerator
 
+from bloom.git import get_current_branch
+
+from bloom.logging import info
+
 
 class DebianGenerator(BloomGenerator):
-    def __init__(self):
-        pass
+    title = 'debian'
+    description = "Generates debians from the catkin meta data"
+
+    def prepare_arguments(self, parser):
+        # Add command line arguments for this generator
+        add = parser.add_argument
+        add('-i', '--debian-inc', help="debian increment number", default='0')
+        add('-s', '--src-branch', default=None,
+            help="branch to branch from and create debian")
+        BloomGenerator.prepare_arguments(self, parser)
+
+    def handle_arguments(self, args):
+        self.interactive = not args.non_interactive
+        self.debian_inc = args.debian_inc
+        self.src = args.src if args.src is not None else get_current_branch()
+
+    def summarize(self):
+        info("Generating source debs for ")
+        info("Debian Incremental Version: " + str(self.debian_inc))
