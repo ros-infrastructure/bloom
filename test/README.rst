1f84a0838028e91480321a28b165e5617c521d2f
William Woodall <wjwwood@gmail.com>
2012-10-11 13:40:32 -0700

    Cleaned up tests folder.

--- test/README.rst
+++
@@ -1,4 +0,0 @@
-bloom tests
-===========
-
-run `make test` or `nosetests` or `nosetests test/testname` from the bloom main directory to run tests. Requires python-nose to be installed.
--- test/setup_test_env.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-# must use bash for BASH_SOURCE, and must not try to get rid of /./
-# test in this folder, parent folder, and grandparent folder
-BASEDIR=`pwd`/$(dirname $BASH_SOURCE)
-echo export PYTHONPATH=$BASEDIR/../src:$PYTHONPATH
-export PYTHONPATH=$BASEDIR/../src:$PYTHONPATH
-echo export PATH=$BASEDIR/../bin:$PATH
-export PATH=$BASEDIR/../bin:$PATH
