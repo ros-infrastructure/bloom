46829435c2b2906179375d328fa437542ba75ca1
William Woodall <wjwwood@gmail.com>
2012-10-23 14:22:12 -0700

    Releasing bloom version 0.2.5
    * Made the version detection in bloom more robust
    * Fixed a bug related to tracking branches
    * Added a script to publish the docs to github

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bloom',
-    version='0.2.4',
+    version='0.2.5',
     packages=find_packages(exclude=['test']),
     package_data={
         'bloom.generators.debian':
