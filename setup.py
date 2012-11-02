abb2e7e7b2402d894cc5549aa8b89889cc1e0518
William Woodall <wjwwood@gmail.com>
2012-11-02 11:12:27 -0700

    Releasing bloom version 0.2.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bloom',
-    version='0.2.8',
+    version='0.2.9',
     packages=find_packages(exclude=['test']),
     package_data={
         'bloom.generators.debian':
