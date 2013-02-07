0cf1e74f72ff4b92e3c7f074a91d6b29472d1173
William Woodall <wjwwood@gmail.com>
2013-02-06 16:11:10 -0800

    Added some convenience to the i/o colorization

--- bloom/logging.py
+++ bloom/logging.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import os
+import string
 import sys
 
 import functools
@@ -24,13 +26,18 @@ def enable_ANSI_colors():
     Populates the global module dictionary `ansi` with ANSI escape sequences.
     """
     global _ansi
-    colors = [
+    color_order = [
         'black', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white'
     ]
+    short_colors = {
+        'black': 'k', 'red': 'r', 'green': 'g', 'yellow': 'y', 'blue': 'b',
+        'purple': 'p', 'cyan': 'c', 'white': 'w'
+    }
     _ansi = {
-        'escape': '\033', 'reset': 0,
-        'boldon': 1, 'italicson': 3, 'ulon': 4, 'invon': 7,
-        'boldoff': 22, 'italicsoff': 23, 'uloff': 24, 'invoff': 27,
+        'escape': '\033', 'reset': 0, '|': 0,
+        'boldon': 1, '!': 1, 'italicson': 3, '/': 3, 'ulon': 4, '_': 4,
+        'invon': 7, 'boldoff': 22, 'italicsoff': 23,
+        'uloff': 24, 'invoff': 27
     }
 
     # Convert plain numbers to escapes
@@ -39,12 +46,21 @@ def enable_ANSI_colors():
             _ansi[key] = '{0}[{1}m'.format(_ansi['escape'], _ansi[key])
 
     # Foreground
-    for index, color in enumerate(colors):
-        _ansi[color + 'f'] = '{0}[{1}m'.format(_ansi['escape'], 30 + index)
+    for index, color in enumerate(color_order):
+        _ansi[color] = '{0}[{1}m'.format(_ansi['escape'], 30 + index)
+        _ansi[color + 'f'] = _ansi[color]
+        _ansi[short_colors[color] + 'f'] = _ansi[color + 'f']
 
     # Background
-    for index, color in enumerate(colors):
+    for index, color in enumerate(color_order):
         _ansi[color + 'b'] = '{0}[{1}m'.format(_ansi['escape'], 40 + index)
+        _ansi[short_colors[color] + 'b'] = _ansi[color + 'b']
+
+    # Fmt sanitizers
+    _ansi['atexclimation'] = '@!'
+    _ansi['atfwdslash'] = '@/'
+    _ansi['atunderscore'] = '@_'
+    _ansi['atbar'] = '@|'
 
 
 def disable_ANSI_colors():
@@ -73,7 +89,7 @@ def enable_debug(state=True):
     _debug = state
 
 # Default to debug off
-enable_debug(False)
+enable_debug('DEBUG' in os.environ)
 
 
 def enable_debug_indent(state=True):
@@ -184,3 +200,30 @@ def error(msg, file=None, end='\n', use_prefix=True, exit=False):
     if not _quiet:
         print(msg, file=file, end=end)
     return msg
+
+
+class ColorTemplate(string.Template):
+    delimiter = '@'
+
+
+def sanitize(msg):
+    """Sanitizes the existing msg, use before adding color annotations"""
+    msg = msg.replace('@', '@@')
+    msg = msg.replace('{', '{{')
+    msg = msg.replace('}', '}}')
+    msg = msg.replace('@@!', '@{atexclimation}')
+    msg = msg.replace('@@/', '@{atfwdslash}')
+    msg = msg.replace('@@_', '@{atunderscore}')
+    msg = msg.replace('@@|', '@{atbar}')
+    return msg
+
+
+def fmt(msg):
+    """Replaces color annotations with ansi escape sequences"""
+    global _ansi
+    msg = msg.replace('@!', '@{boldon}')
+    msg = msg.replace('@/', '@{italicson}')
+    msg = msg.replace('@_', '@{ulon}')
+    msg = msg.replace('@|', '@{reset}')
+    t = ColorTemplate(msg)
+    return t.substitute(_ansi) + ansi('reset')
