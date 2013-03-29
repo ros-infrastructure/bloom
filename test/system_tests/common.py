261e6100b6c6391a970b0802766db79a9d77967b
William Woodall <wjwwood@gmail.com>
2013-03-29 15:53:11 -0700

    Style

--- test/system_tests/common.py
+++ test/system_tests/common.py
@@ -12,12 +12,12 @@ from ..utils.common import user
 
 
 def create_release_repo(upstream_url, upstream_type, upstream_branch='',
-    rosdistro='groovy'):
+                        rosdistro='groovy'):
     user('mkdir foo_release')
     with change_directory('foo_release'):
         user('git init .')
         answers = ['y', 'foo', upstream_url, upstream_type,
-            '', '', upstream_branch, rosdistro]
+                   '', '', upstream_branch, rosdistro]
         with bloom_answer(answers):
             user('git-bloom-config new ' + str(rosdistro))
         url = 'file://' + os.getcwd()
