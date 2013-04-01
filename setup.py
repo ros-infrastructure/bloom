f655a325ce06bd0298107785b9b150a3af7dee24
William Woodall <william@osrfoundation.org>
2013-04-01 14:04:00 -0700

    0.3.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bloom',
-    version='0.3.2',
+    version='0.3.3',
     packages=find_packages(exclude=['test']),
     package_data={
         'bloom.generators.debian':
