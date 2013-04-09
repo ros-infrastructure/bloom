c1263b0531fd092478b8fd3193505637045834c1
Dirk Thomas <dthomas@willowgarage.com>
2013-04-09 16:33:59 -0700

    fix version deps

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'rosdep >= 0.10.3',
         'rospkg >= 1.0.6',
         'vcstools >= 0.1.22',
-        'catkin-pkg >= 0.1.10',
+        'catkin-pkg >= 0.1.11',
         'python-dateutil',
         'distribute'
     ],
