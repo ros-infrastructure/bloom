5305e9749e17a2162939c4f29d2400e216fe94cb
Tully Foote <tfoote@osrfoundation.org>
2016-03-04 17:55:29 -0800

    Add support for building for debian platforms into the default actions.
    They will fail gracefully for the older rosdistros which do not have
    debian platform targets.

--- bloom/config.py
+++ bloom/config.py
@@ -200,7 +200,9 @@ DEFAULT_TEMPLATE = {
         'git-bloom-generate -y rosrelease :{ros_distro}'
         ' --source upstream -i :{release_inc}',
         'git-bloom-generate -y rosdebian --prefix release/:{ros_distro}'
-        ' :{ros_distro} -i :{release_inc}',
+        ' :{ros_distro} -i :{release_inc} --os-name ubuntu',
+        'git-bloom-generate -y rosdebian --prefix release/:{ros_distro}'
+        ' :{ros_distro} -i :{release_inc} --os-name debian --os-not-required',
         'git-bloom-generate -y rosrpm --prefix release/:{ros_distro}'
         ' :{ros_distro} -i :{release_inc}'
     ]
