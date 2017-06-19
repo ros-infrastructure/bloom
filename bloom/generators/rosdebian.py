2185388edf8b336ed10de3325adca1a6ac5d4f7e
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-06-19 10:51:49 -0400

    Increase the "severity" of the comment so this is revisited sooner.

--- bloom/generators/rosdebian.py
+++ bloom/generators/rosdebian.py
@@ -86,7 +86,8 @@ class RosDebianGenerator(DebianGenerator):
             fallback_resolver=fallback_resolver
         )
         subs['Package'] = rosify_package_name(subs['Package'], self.rosdistro)
-        # Add workspace package to runtime and buildtime dependencies.
+        # XXX Add workspace package to runtime and buildtime dependencies.
+        # This is a ros2-specific hack that should be factored better into the buildfarm/bloom release process.
         if package.name not in ['ament_cmake_core', 'ament_package']:
             workspace_pkg_name = rosify_package_name('ros-workspace', self.rosdistro)
             subs['BuildDepends'].append(workspace_pkg_name)
