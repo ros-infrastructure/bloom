8013a1a05025397e7273f8135fd284237d0d284e
Scott K Logan <logans@cottsay.net>
2019-01-31 12:56:31 -0800

    Set Git author/committer within tests (#514)
    This removes the environmental requirement that Git be configured prior
    to running the tests.
    
    Signed-off-by: Scott K Logan <logans@cottsay.net>

--- test/__init__.py
+++ test/__init__.py
@@ -5,3 +5,11 @@ if 'PATH' in os.environ:
     scripts = os.path.join(os.path.dirname(__file__), '..', 'scripts')
     scripts = os.path.abspath(scripts)
     os.environ['PATH'] = scripts + ':' + os.environ['PATH']
+
+user_email = 'test@example.com'
+user_name = 'Test User'
+
+os.environ.setdefault('GIT_AUTHOR_NAME', user_name)
+os.environ.setdefault('GIT_AUTHOR_EMAIL', user_email)
+os.environ.setdefault('GIT_COMMITTER_NAME', user_name)
+os.environ.setdefault('GIT_COMMITTER_EMAIL', user_email)
