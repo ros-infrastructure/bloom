9b5b8be68068ec1f09ad9bb128a9c860036d297f
Steven! Ragnarök <steven@nuclearsandwich.com>
2019-02-28 20:30:33 -0800

    Evaluate dependency conditions when evaluating templates.

--- bloom/generators/debian/generator.py
+++ bloom/generators/debian/generator.py
@@ -312,10 +312,17 @@ def generate_substitutions_from_package(
     # Installation prefix
     data['InstallationPrefix'] = installation_prefix
     # Resolve dependencies
-    depends = package.run_depends + package.buildtool_export_depends
-    build_depends = package.build_depends + package.buildtool_depends + package.test_depends
+    package.evaluate_conditions(os.environ)
+    depends = [
+        dep for dep in (package.run_depends + package.buildtool_export_depends)
+        if dep.evaluated_condition]
+    build_depends = [
+        dep for dep in (package.build_depends + package.buildtool_depends + package.test_depends)
+        if dep.evaluated_condition]
 
-    unresolved_keys = depends + build_depends + package.replaces + package.conflicts
+    unresolved_keys = [
+        dep for dep in (depends + build_depends + package.replaces + package.conflicts)
+        if dep.evaluated_condition]
     # The installer key is not considered here, but it is checked when the keys are checked before this
     resolved_deps = resolve_dependencies(unresolved_keys, os_name,
                                          os_version, ros_distro,
