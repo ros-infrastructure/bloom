f8e3bdfbbd57ff220079906f563ed8295bc4bfd6
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-04-06 15:27:35 -0700

    Format comment block to fit line length.

--- bloom/commands/release.py
+++ bloom/commands/release.py
@@ -679,9 +679,12 @@ Increasing version of package(s) in repository `{repository}` to `{version}`:
             # Check if a fork already exists on the user's account
 
             try:
-                # There are a lot of forks of the ros/rosdistro repository so listing those forks takes a very long time.
-                # Let's try a little shortcut by checking if the repository of the same name owned by the current user
-                # if that repository exists and is in the same fork network as the target repo let's take it.
+                # There are a lot of forks of the ros/rosdistro repository so
+                # listing those forks takes a very long time.
+                # Let's try a little shortcut by checking if the repository of
+                # the same name owned by the current user, if that repository
+                # exists and is in the same fork network as the target repo
+                # let's take it.
                 # If it is not, we still fall back to listing forks.
                 target_repo = gh.get_repo(base_info['org'], base_info['repo'])
                 target_repo_source = target_repo['full_name']
