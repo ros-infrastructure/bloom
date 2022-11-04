c46231e526cd88ecec70c7d03a67bae27e9b429a
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-11-04 12:03:32 -0700

    Use format instead of f-string.
    Cannot wait for the end of Python2 support.

--- bloom/commands/release.py
+++ bloom/commands/release.py
@@ -927,7 +927,7 @@ def _perform_release(
         info(fmt("@{bf}@!==> @|@!") + str(cmd))
         subprocess.check_call(cmd, shell=True)
         # Dry run will authenticate, but not push
-        cmd = f'git push --set-upstream origin {BLOOM_CONFIG_BRANCH} --dry-run'
+        cmd = 'git push --set-upstream origin {0} --dry-run'.format(BLOOM_CONFIG_BRANCH)
         info(fmt("@{bf}@!==> @|@!") + str(cmd))
         subprocess.check_call(cmd, shell=True)
     except subprocess.CalledProcessError:
