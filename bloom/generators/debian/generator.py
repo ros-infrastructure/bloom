057af7389dbec4f4d61f3061030b203aea8a8e00
Jose Luis Rivero <jrivero@honurobotics.com>
2025-09-24 15:52:45 +0200

    Support empy4 together with empy3
    Use a wrapper function to make the em.expand calls.
    
    Signed-off-by: Jose Luis Rivero <jrivero@honurobotics.com>

--- bloom/generators/debian/generator.py
+++ bloom/generators/debian/generator.py
@@ -78,6 +78,8 @@ from bloom.logging import info
 from bloom.logging import is_debug
 from bloom.logging import warning
 
+from bloom.util import expand_template_em
+
 from bloom.commands.git.patch.common import get_patch_config
 from bloom.commands.git.patch.common import set_patch_config
 
@@ -101,12 +103,6 @@ try:
 except ImportError as err:
     debug(traceback.format_exc())
     error("rosdistro was not detected, please install it.", exit=True)
-
-try:
-    import em
-except ImportError:
-    debug(traceback.format_exc())
-    error("empy was not detected, please install it.", exit=True)
 
 # Drop the first log prefix for this command
 enable_drop_first_log_prefix(True)
@@ -514,7 +510,7 @@ def __process_template_folder(path, subs):
         info("Expanding '{0}' -> '{1}'".format(
             os.path.relpath(item),
             os.path.relpath(template_path)))
-        result = em.expand(template, **subs)
+        result = expand_template_em(template, subs)
         # Don't write an empty file
         if len(result) == 0 and \
            os.path.basename(template_path) in ['copyright']:
