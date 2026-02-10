d5c718317cad3d12dbe33374ebb4b22c883371f8
Scott K Logan <logans@cottsay.net>
2026-02-10 11:56:03 -0600

    Pin setuptools < 82 for pkg_resources (#757)
    We clearly need to make some progress moving away from pkg_resources,
    but we should pin this for now to unblock builds.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     },
     install_requires=[
         'catkin_pkg >= 0.4.3',
-        'setuptools',
+        'setuptools < 82',
         'empy < 4',
         'packaging',
         'python-dateutil',
